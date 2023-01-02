59fec4e1c9e52d4fa5f5666b1f07077f14aedfaf
akshaytalanki19 <110332292+akshaytalanki19@users.noreply.github.com>
2023-01-02 10:39:41 +0530

    Delete part1/Part1 directory

--- part1/Part1/Ex-1.1-1.2/courseinfo/README.md
+++
@@ -1,5 +0,0 @@
-Repository for solution to [Part 1 Intro to React Exercises](https://fullstackopen.com/en/part1/introduction_to_react#exercises)
-
-### Resulting UI:
-
-![1.1_screenshot](../assets/courseinfo.png)
--- part1/Part1/Ex-1.1-1.2/courseinfo/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-// @author: Ray Blandford
-// @date: Feb 5, 2020
-ReactDOM.render(<App />, document.getElementById('root'));
